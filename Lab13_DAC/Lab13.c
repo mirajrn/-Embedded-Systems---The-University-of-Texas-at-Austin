3a83eb28b2ee6eb0aa0f9ca3d70f759446719568
mirajrn <rashedcurut@gmail.com>
2017-10-03 20:46:10 +0200

    Upload Codes

---
+++ Lab13_DAC/Lab13.c
@@ -0,0 +1,71 @@
+// Lab13.c
+// Runs on LM4F120 or TM4C123
+// Use SysTick interrupts to implement a 4-key digital piano
+// edX Lab 13 
+// Daniel Valvano, Jonathan Valvano
+// March 13, 2014
+// Port B bits 3-0 have the 4-bit DAC
+// Port E bits 3-0 have 4 piano keys
+
+#include "..//tm4c123gh6pm.h"
+#include "Sound.h"
+#include "Piano.h"
+#include "TExaS.h"
+
+// basic functions defined at end of startup.s
+void DisableInterrupts(void); // Disable interrupts
+void EnableInterrupts(void);  // Enable interrupts
+void delay(unsigned long msec);
+int main(void){ // Real Lab13 
+	unsigned long current;
+	const int D=8518;
+	const int C=1000000000/(523*16*12.5);
+	const int G=1000000000/(783*16*12.5);
+	const int E=1000000000/(659*16*12.5);
+	      
+	
+  TExaS_Init(SW_PIN_PE3210, DAC_PIN_PB3210,ScopeOn); // activate grader and set system clock to 80 MHz
+// PortE used for piano keys, PortB used for DAC        
+  Sound_Init(); // initialize SysTick timer and DAC
+  Piano_Init();
+  EnableInterrupts();  // enable after all initialization are done
+	
+  while(1){                
+		// input from keys to select tone
+		current = Piano_In();
+		if (0x01==current) {
+			
+			Sound_Tone(C);
+		
+		} else if (0x02==current) {
+			
+			Sound_Tone(D);
+			
+		} else if (0x04==current) {
+			
+			Sound_Tone(E);
+			
+		} else if (0x08==current) {
+			
+			Sound_Tone(G);
+			
+		} else {
+			Sound_Off();
+		}
+		
+		delay(10);
+  }            
+}
+
+// Inputs: Number of msec to delay
+// Outputs: None
+void delay(unsigned long msec){ 
+  unsigned long count;
+  while(msec > 0 ) {  
+    count = 16000;    // about 1ms
+    while (count > 0) { 
+      count--;
+    } // This while loop takes approximately 3 cycles
+    msec--;
+  }
+}
---
+++ Lab6_BranchingFunctionsDelays/BranchingFunctionsDelays.c
@@ -0,0 +1,76 @@
+// BranchingFunctionsDelays.c Lab 6
+// Runs on LM4F120/TM4C123
+// Use simple programming structures in C to 
+// toggle an LED while a button is pressed and 
+// turn the LED on when the button is released.  
+// This lab will use the hardware already built into the LaunchPad.
+// Authors: Basic Version: Daniel Valvano, Jonathan Valvano
+// Authors: Modification for the project: Rashed Hossain Miraj 
+
+// built-in connection: PF0 connected to negative logic momentary switch, SW2
+// built-in connection: PF1 connected to red LED
+// built-in connection: PF2 connected to blue LED
+// built-in connection: PF3 connected to green LED
+// built-in connection: PF4 connected to negative logic momentary switch, SW1
+
+#include "TExaS.h"
+
+#define GPIO_PORTF_DATA_R       (*((volatile unsigned long *)0x400253FC))
+#define GPIO_PORTF_DIR_R        (*((volatile unsigned long *)0x40025400))
+#define GPIO_PORTF_AFSEL_R      (*((volatile unsigned long *)0x40025420))
+#define GPIO_PORTF_PUR_R        (*((volatile unsigned long *)0x40025510))
+#define GPIO_PORTF_DEN_R        (*((volatile unsigned long *)0x4002551C))
+#define GPIO_PORTF_AMSEL_R      (*((volatile unsigned long *)0x40025528))
+#define GPIO_PORTF_PCTL_R       (*((volatile unsigned long *)0x4002552C))
+#define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108))
+#define SYSCTL_RCGC2_GPIOF      0x00000020  // port F Clock Gating Control
+
+// basic functions defined at end of startup.s
+void DisableInterrupts(void); // Disable interrupts
+void EnableInterrupts(void);  // Enable interrupts
+
+void PortF_Init(void);
+void Delay100ms(unsigned long);
+unsigned long SW1;  // input from PF4
+
+int main(void){ unsigned long volatile delay;
+  TExaS_Init(SW_PIN_PF4, LED_PIN_PF2);  // activate grader and set system clock to 80 MHz
+  // initialization goes here
+	PortF_Init();
+  EnableInterrupts();           // enable interrupts for the grader
+  while(1){
+    // body goes here
+		Delay100ms(1);
+		SW1 = GPIO_PORTF_DATA_R&0x10;     // read PF4 into SW1
+		if(!SW1){                     // switch pressed
+      GPIO_PORTF_DATA_R ^= 0x04;       // Toggle pf2 bit
+    }
+		else{
+		GPIO_PORTF_DATA_R|=0X04 ;	
+		}
+  }
+}
+
+void PortF_Init(void){ volatile unsigned long delay;
+  SYSCTL_RCGC2_R |= 0x00000020;     // 1) F clock
+  delay = SYSCTL_RCGC2_R;           // delay         
+  GPIO_PORTF_AMSEL_R = 0x00;        // 3) disable analog function
+  GPIO_PORTF_PCTL_R = 0x00000000;   // 4) GPIO clear bit PCTL  
+  GPIO_PORTF_DIR_R = 0x0E;          // 5) PF4,PF0 input, PF3,PF2,PF1 output   
+  GPIO_PORTF_AFSEL_R = 0x00;        // 6) no alternate function
+  GPIO_PORTF_PUR_R = 0x11;          // enable pullup resistors on PF4,PF0       
+  GPIO_PORTF_DEN_R = 0x1F;          // 7) enable digital pins PF4-PF0   
+  GPIO_PORTF_DATA_R|=0X04 ;	        //initially pf2 is on
+}
+
+
+void Delay100ms(unsigned long time){
+  unsigned long i;
+  while(time > 0){
+    i = 1333333;  // this number means 100ms
+    while(i > 0){
+      i = i - 1;
+    }
+    time = time - 1; // decrements every 100 ms
+  }
+}
